e4caedcc5d534d075043de1ce8dbc1bd7f4902ff
isaacyu <isaac_yusir@yahoo.com.hk>
2017-08-04 11:10:17 +0800

    Update vincent.js

--- vincent.js
+++ vincent.js
@@ -1,9 +1,9 @@
 // link for direct use:
-// 
+//    https://rawgit.com/isaacyu/katex-translate/master/vincent.js
 // link to make above link from github
 //    https://rawgit.com/
 // github link
-// 
+//    https://github.com/isaacyu/katex-translate/edit/master/vincent.js
 
 function vincentReplaceBefore(){
   console.log('load:vincent.js:vincentReplaceBefore')
