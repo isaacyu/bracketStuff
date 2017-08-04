6873050e3a2b3dc4a0d776d62cdf77f6e91821c9
isaacyu <isaac_yusir@yahoo.com.hk>
2017-08-04 11:07:03 +0800

    Create vincent.js

---
+++ vincent.js
@@ -0,0 +1,14 @@
+// link for direct use:
+// 
+// link to make above link from github
+//    https://rawgit.com/
+// github link
+// 
+
+function vincentReplaceBefore(){
+  console.log('load:vincent.js:vincentReplaceBefore')
+}
+
+function vincentReplaceAfter(){
+  console.log('load:vincent.js:vincentReplaceAfter')
+}
